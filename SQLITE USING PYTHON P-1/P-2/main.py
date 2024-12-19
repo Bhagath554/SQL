1805012c5f711bbab2a9e39387d0f76508fd7ed2
sureshsk9 <suresh@settleking.com>
2024-12-19 18:48:51 +0530

    SQLite using python cl -1,p-1

---
+++ SQLITE USING PYTHON P-1/P-2/main.py
@@ -0,0 +1,8 @@
+print('What is the correct answer of the given Database -\n 1)1.RELATIONAL DATABASE \n 2.NON-RELATIONAL DATABASE')
+answer = int(input("Enter Answer:"))
+if answer == 1:
+    print("You guessed it Right")
+
+else:
+    print("You Guessed it wrong")
+print("\n Please tell your Mentor Why you Guessed this? ")
