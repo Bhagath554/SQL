42130e4903a5443b40a6c553fa3ede5abadb489d
sureshsk9 <suresh@settleking.com>
2025-03-25 18:59:25 +0530

    Step throat part1

---
+++ Step Throat Part 1(BT)/main.py
@@ -0,0 +1,37 @@
+def find(a,b):
+
+    if a++1:
+            prob_a = 0.2
+            if b==1:
+                    prob_bga = 0.85
+            elif b==2:
+                    prob_bga = 0.15
+            else:
+                    print("Invalid Choice")
+            prob_ab =prob_a*prob_bga
+            print("Probability of  given a ",prob_bga)
+            print("Probability of both events occuring",prob_ab)
+    elif a==2:
+        prob_a = 0.8
+        if b==1:
+                prob_bga = 0.02
+        elif b==2:
+                prob_bga = 0.98
+        else:
+                    print("Invalid Choice")
+        prob_ab =prob_a*prob_bga
+        print("Probability of  given a ",prob_bga)
+        print("Probability of both events occuring",prob_ab)
+    else :
+        print("Invalid choice")
+print("Lets calculate probability.please enter your choice")
+print("Person has step throat \n 1)Yes \n 2)No")
+a = int(input("Enter your choice(1/2) :"))
+
+print("Person tested positive \n 1)Yes \n 2)No")
+b = int(input("Enter your choice(1/2) :"))
+
+print("Probability of both events is :")
+find(a,b)
+
+
