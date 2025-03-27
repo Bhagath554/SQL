03981e5e053896ace0bb22ec8f27b080cae59795
sureshsk9 <suresh@settleking.com>
2025-03-27 19:02:27 +0530

    What are your WC

--- Calls Poisson/main.py
+++
@@ -1,7 +0,0 @@
-import scipy.stats as stats
-
-prob1 = 1-stats.poisson.cdf(20,15)
-print(prob1)
-
-prob2 = stats.poisson.cdf(21,15) - stats.poisson.cdf(16,20)
-print(prob2)
---
+++ What Are your WC/main.py
@@ -0,0 +1,11 @@
+red = 1
+blue = 6
+white = 3
+total = red+blue+white
+prob_a = white/total
+prob_b = white/total
+prob_bga = prob_b
+print("Probability that second ball is white and first ball is also white is ")
+print(round((prob_bga),2))
+
+    
